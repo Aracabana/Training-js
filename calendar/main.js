00866fe9a6f8e0000fdb74132d659a14a338cde6
Julia Kadilnikova <aracabana@gmail.com>
2020-04-14 01:56:07 +0300

    updates

--- calendar/main.js
+++ calendar/main.js
@@ -31,47 +31,35 @@ const calendarData = {
     }
 };
 function Calendar() {
-    // const monthNames = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-    // const dayNames = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-    let daysByWeek = [];
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const currentDate = new Date().getDate();
-    // const monthsLength = calendarData.setMonthLength();
-    // const getMonthLength = function () {
-    //     return 33 - new Date(currentYear, currentMonth, 33).getDate();
-    // };
-    const getFirstDay = function () {
-        if (new Date(currentYear, currentMonth, 1).getDay() - 1 === -1) {
-            return 6;
-        }
-        return new Date(currentYear, currentMonth, 1).getDay() - 1;
+    let daysByWeeks = [];
+    const getFirstDay = function (monthNumber, year) {
+        if (new Date(year, monthNumber, 1).getDay() - 1 === -1) {return 6;}
+        return new Date(year, monthNumber, 1).getDay() - 1;
     };
-    const getLastDay = function () {
-        if (new Date(currentYear, currentMonth, calendarData.setMonthLength()[currentMonth]).getDay() - 1 === -1) {
-            return 6;
-        }
-        return new Date(currentYear, currentMonth, calendarData.setMonthLength()[currentMonth]).getDay() - 1;
+    const getLastDay = function (monthNumber, year) {
+        if (new Date(year, monthNumber, calendarData.setMonthLength()[monthNumber]).getDay() - 1 === -1) {return 6;}
+        return new Date(year, monthNumber, calendarData.setMonthLength()[monthNumber]).getDay() - 1;
     };
-    const setDays = function () {
+    const setDays = function (monthNumber, year) {
         let days = [];
         //записываем дни месяца в массив
-        for (let i = 1; i <= calendarData.setMonthLength()[currentMonth]; i++) {days.push(i);}
+        for (let i = 1; i <= calendarData.setMonthLength()[monthNumber]; i++) {days.push(i);}
         //сдвигаем первый день месяца на нужную позицию
-        for (let i = 0; i < getFirstDay(); i++) {days.unshift('');}
+        for (let i = 0; i < getFirstDay(monthNumber, year); i++) {days.unshift('');}
         //добавляем пустые элементы в конец массива, чтобы длина массива дней была кратна 7
         const emptyCellsCount = calendarData.dayNames.length - (days.length % 7);
-        if (getLastDay() !== 6) {for (let i = 0; i < emptyCellsCount; i++) {days.push('');}}
+        if (getLastDay(monthNumber, year) !== 6) {for (let i = 0; i < emptyCellsCount; i++) {days.push('');}}
         //разбиваем массив дней на недели
-        while(days.length) {daysByWeek.push(days.splice(0, calendarData.dayNames.length));}
-        return daysByWeek;
+        while(days.length) {daysByWeeks.push(days.splice(0, calendarData.dayNames.length));}
+        return daysByWeeks;
     };
-    const createHTML = function () {
-        const monthName = createNewElementAppend('h1', app, 'calendar-month');
-        monthName.innerText = calendarData.monthNames[currentMonth] + ', ' + currentYear;
-        const table = createNewElementAppend('table', app, 'calendar');
+    const createHTML = function (monthNumber, year, isPreviousMonth, isCurrentMonth) {
+        const wrapper = createNewElementAppend('div', app, 'calendar-wrapper');
+        const monthName = createNewElementAppend('h1', wrapper, 'calendar-month');
+        monthName.innerText = calendarData.monthNames[monthNumber] + ', ' + year;
+        const table = createNewElementAppend('table', wrapper, 'calendar');
         createHeaderHTML(table);
-        createBodyHTML(table);
+        createBodyHTML(table, monthNumber, year, isPreviousMonth, isCurrentMonth);
     };
     const createHeaderHTML = function (table) {
         const thead = createNewElementPrepend('thead', table, '');
@@ -81,27 +69,33 @@ function Calendar() {
             theadDay.innerText = calendarData.dayNames[i];
         }
     };
-    const createBodyHTML = function (table) {
+    const createBodyHTML = function (table, monthNumber, year, isPreviousMonth, isCurrentMonth) {
         const tbody = createNewElementAppend('tbody', table, '');
-        const tbodyData = setDays();
+        const tbodyData = setDays(monthNumber, year);
         for (let i = 0; i < tbodyData.length; i++) {
             let tbodyTr = createNewElementAppend('tr', tbody, '');
             for (let j = 0; j < tbodyData[i].length; j++) {
                 let tbodyDay = createNewElementAppend('td', tbodyTr, 'calendar-day');
                 let tbodyDayDate = createNewElementAppend('span', tbodyDay, 'calendar-day-date');
                 tbodyDayDate.innerText = tbodyData[i][j];
-                if (tbodyData[i][j] === currentDate) {
+                if (isCurrentMonth && tbodyData[i][j] === new Date().getDate()) {
                     tbodyDay.classList.add('current');
                 }
-                if (tbodyData[i][j] < currentDate) {
+                if ((isCurrentMonth && tbodyData[i][j] < new Date().getDate()) || isPreviousMonth) {
                     tbodyDay.classList.add('before');
                 }
             }
         }
     };
-    this.create = function () {
-        createHTML();
+    this.create = function (monthNumber, year, isPreviousMonth, isCurrentMonth) {
+        createHTML(monthNumber, year, isPreviousMonth, isCurrentMonth);
     }
 }
-let currentCalendar = new Calendar().create();
-console.log(calendarData.setMonthLength());
+for (let i = 0; i < 12; i++) {
+    let isCurrentMonth = false;
+    if (i === new Date().getMonth()){isCurrentMonth = true;}
+    let isPreviousMonth = false;
+    if (i < new Date().getMonth()){isPreviousMonth = true;}
+    new Calendar().create(i, new Date().getFullYear(), isPreviousMonth, isCurrentMonth);
+}
+
